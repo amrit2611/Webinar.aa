b1245a133f13477950c2f5d5a07d93555df0e41e
amrit <amrit@aviac.in>
2025-04-09 03:07:34 +0530

    made the options card more responsive

--- src/components/answers/Card.jsx
+++ src/components/answers/Card.jsx
@@ -1,11 +1,11 @@
 export function Card() {
-    return <div className="border border-gray-400 rounded-2xl h-96 bg-white col-span-12 md:col-span-3 shadow-lg md:-translate-y-20 mb-10 md:block hidden p-2 overflow-hidden">
-        <img src="src/components/answers/icons/UserProfileImage.png" alt="profile_img" className="w-20 mx-auto mt-10" />
-        <div className="m-5">
-            <div className="flex justify-center font-bold">Shinchan Nohara</div>
-            <div className="flex justify-center">shinchan@y.com</div>
-            <div className="flex justify-center">656546588</div>
-            <div className="flex justify-center">Somewhere, Japan</div>
+    return <div className="border border-gray-200 rounded-2xl h-84 bg-white col-span-12 md:col-span-3 shadow-lg md:-translate-y-20 mb-10 md:block hidden p-2 overflow-hidden">
+        <img src="src/components/answers/icons/UserProfileImage.png" alt="profile_img" className="w-24 mx-auto mt-10" />
+        <div className="mt-6 mx-2 overflow-auto">
+            <div className="text-center font-bold">Shinchan Nohara</div>
+            <div className="text-center text-gray-600 text-sm mt-4">shinchan@y.com</div>
+            <div className="text-center text-gray-600 text-sm mb-2">656546588</div>
+            <div className="text-center text-gray-600 text-sm mb-20">Tokyo, Japan</div>
         </div>
     </div>
 }
