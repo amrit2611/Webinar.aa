c4e3ba67c99154c7ad51ac3a9aeeafae6a4b52c6
amrit <amrit@aviac.in>
2025-04-11 23:05:48 +0530

    profile image

--- src/components/answers/Card.jsx
+++ src/components/answers/Card.jsx
@@ -1,6 +1,6 @@
 export function Card() {
     return <div className="border border-gray-200 rounded-2xl h-84 bg-white col-span-12 md:col-span-3 shadow-lg md:-translate-y-20 mb-10 md:block hidden p-2 overflow-hidden">
-        <img src="src/components/answers/icons/UserProfileImage.png" alt="profile_img" className="w-24 mx-auto mt-10" />
+        <img src="src/components/answers/icons/UserProfileImage.svg" alt="profile_img" className="w-24 mx-auto mt-10" />
         <div className="mt-6 mx-2 overflow-auto">
             <div className="text-center font-bold">Shinchan Nohara</div>
             <div className="text-center text-gray-600 text-sm mt-4">shinchan@y.com</div>
